b7d4d5feb646124a70c5ce03c22c4d6f677d61d7
sunanswa <sunanswapro@gmail.com>
2025-07-23 22:38:04 +0700

    Updated package.json

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    <Toaster />
   </StrictMode>
 );
